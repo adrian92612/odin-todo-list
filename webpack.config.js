9a2ce4390367e8108cc257f1b613f0756cf63bac
adrian92612 <adrian.villamin0612@gmail.com>
2023-11-21 20:24:54 +0800

    added css loader, htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
 
 module.exports = {
   mode: `development`,
@@ -7,4 +8,19 @@ module.exports = {
     path: path.resolve(__dirname, `dist`),
     filename: `bundle.js`,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [`style-loader`, `css-loader`],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: `index.html`,
+      template: `src/template.html`,
+    }),
+  ],
 };
