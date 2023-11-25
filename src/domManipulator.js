471d7db4a5239ba4ebe5eb5b310915c996361396
adrian92612 <adrian.villamin0612@gmail.com>
2023-11-25 20:52:05 +0800

    blank project names now allowed

--- src/domManipulator.js
+++ src/domManipulator.js
@@ -7,6 +7,11 @@ function addProjectName(e) {
   e.preventDefault();
   const newProjectName = document.createElement("button");
   newProjectName.innerText = e.target.children[0].value.trim(); // e.target = form children[0] = input
+
+  if (newProjectName.innerText == "" || newProjectName == null) {
+    e.target.reset();
+    return;
+  }
   newProjectName.addEventListener("click", () => {
     setCurrentTab(newProjectName.innerText);
   });
